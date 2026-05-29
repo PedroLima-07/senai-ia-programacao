2eabf258b2279dff4833c25c352c7bbe51d78f0a
PedroLima-07 <pedrocardosolima20@gmail.com>
2026-05-29 19:48:55 -0300

    update da lista de exercicios

--- Lista_de_Exercicios_05/Ex10.py
+++ Lista_de_Exercicios_05/Ex10.py
@@ -7,3 +7,45 @@
 # receber resposta
 # transformar resposta em voz
 # salvar áudio
+
+from openai import OpenAI
+from gtts import gTTS
+
+# Sua chave da NVIDIA
+cliente = OpenAI(
+    api_key="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",
+    base_url="https://integrate.api.nvidia.com/v1"
+)
+
+palavra = input("Qual a sua duvida de hoje?\n")
+
+# Gera resposta da IA
+resposta = cliente.chat.completions.create(
+    model="meta/llama-3.1-8b-instruct",
+
+    messages=[
+        {
+            "role": "system",
+            "content": "Seja fiel ao texto para ser narrado em voz alta"
+        },
+
+        {
+            "role": "user",
+            "content": palavra
+        }
+    ]
+)
+
+texto = resposta.choices[0].message.content
+
+print("\nResposta:\n")
+print(texto)
+# Converter texto em voz
+tts = gTTS(
+    text=texto,
+    lang="pt"
+)
+
+tts.save("ex10.mp3")
+
+print("\nÁudio salvo como ex10.mp3")
--- Lista_de_Exercicios_05/Ex9.py
+++ Lista_de_Exercicios_05/Ex9.py
@@ -3,3 +3,24 @@
 # peça um texto usando input()
 # transforme em áudio
 # salve como fala.mp3
+from openai import OpenAI
+from gtts import gTTS
+
+# Sua chave da NVIDIA
+cliente = OpenAI(
+    api_key="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",
+    base_url="https://integrate.api.nvidia.com/v1"
+)
+
+palavra = input("Digite o Texto que deseja converter em audio:\n")
+
+
+# Converter texto em voz
+tts = gTTS(
+    text=palavra,
+    lang="pt"
+)
+
+tts.save("ex9.mp3")
+
+print("\nÁudio salvo como ex9.mp3")
