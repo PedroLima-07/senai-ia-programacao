dea59a7d3abe07d8e3cab6607cdc44bacd9a66fd
PedroLima-07 <pedrocardosolima20@gmail.com>
2026-05-29 12:49:59 -0300

    os exercicios 7 , 9 e 10 não estão completos

---
+++ Lista_de_Exercicios_05/Ex10.py
@@ -0,0 +1,9 @@
+# Exercício 10 — IA + Voz
+# Junte:
+# NVIDIA API
+# gTTS
+# O programa deve:
+# enviar pergunta para IA
+# receber resposta
+# transformar resposta em voz
+# salvar áudio
---
+++ Lista_de_Exercicios_05/Ex9.py
@@ -0,0 +1,5 @@
+# Exercício 9 — Texto para Fala
+# Utilizando gTTS:
+# peça um texto usando input()
+# transforme em áudio
+# salve como fala.mp3
