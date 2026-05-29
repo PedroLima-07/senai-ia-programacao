95e8395c0b7e59057b6bffdab427bf470251fba4
PedroLima-07 <pedrocardosolima20@gmail.com>
2026-05-28 21:05:41 -0300

    update

--- Lista_de_Exercicios_05/Ex5.py
+++ Lista_de_Exercicios_05/Ex5.py
@@ -13,24 +13,40 @@ cliente = OpenAI(
     api_key="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",
     base_url="https://integrate.api.nvidia.com/v1"
 )
-pergunta =("Digite 'sair' para encerrar o programa:\n")
-texto = input("Digite o texto que deseja traduzir:\n")
-idioma = input("Digite o idioma para o qual deseja traduzir:\n")
 
-resposta = cliente.chat.completions.create(
+print("\n--Digite 'sair' para encerrar o programa.--\n")
+
+while True:
+
+    texto = input("Digite o texto que deseja traduzir:\n")
+
+    if texto.lower() == "sair":
+        print("Programa encerrado.")
+        break
+
+    idioma = input("Digite o idioma para o qual deseja traduzir:\n")
+
+    resposta = cliente.chat.completions.create(
         model="meta/llama-3.1-8b-instruct",
 
         messages=[
             {
                 "role": "system",
+                "content": "Você é um tradutor profissional, que apenas entrega a tradução solicitada."
+            },
+
+            {
+                "role": "user",
                 "content": (
-                    f"Você é um tradutor profissional. Traduza o seguinte texto para {idioma}: {texto}"
+                    f"Traduza o seguinte texto para {idioma}: {texto}"
                 )
             }
         ]
     )
-print("\nTexto traduzido da IA:\n")
-print(resposta.choices[0].message.content)
+
+    print("\nTexto traduzido pela IA:\n")
+    print(resposta.choices[0].message.content)
+    print("\n---------------------------\n")
         
     
 
